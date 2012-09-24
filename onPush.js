2bb4d2fe81bdf35fe90ca300b48a98dad4064cae
alexzaporozhets <alexzaporozhets@gmail.com>
2012-09-24 12:44:08 +0300

    Update onPush.js
    added PHPUnit execution

--- onPush.js
+++ onPush.js
@@ -6,6 +6,8 @@ function puts(error, stdout, stderr) { sys.puts(stdout) }
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   exec("cd /var/www/TDv3 && git fetch && git pull --rebase >> ~/gitauto.log", puts);
+  // execution PHPUnit
+  exec("cd /var/www/TDv3/tests && phpunit >> ~/phpunit.log", puts);
   res.end('GitHub hook\n');
 }).listen(20081);
 console.log('Server running at http://127.0.0.1:20081/');
